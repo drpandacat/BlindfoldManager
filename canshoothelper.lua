7a35c3c970e9ba9982f35f5977f358b19a15b9bf
Kerkel <134325525+drpandacat@users.noreply.github.com>
2025-04-05 11:53:22 -0400

    Update canshoothelper.lua

--- canshoothelper.lua
+++ canshoothelper.lua
@@ -1,35 +1,45 @@
 --[[
-    Can Shoot Helper by Kerkel
+    CanShoot Helper by Kerkel
     Version 1.0
 ]]
 
 local VERSION = 1
 
 if CanShootHelper then
-    if CanShootHelper.Internal.VERSION > VERSION then
-        return
-    end
+    if CanShootHelper.Internal.VERSION > VERSION then return end
     for _, v in ipairs(CanShootHelper.Internal.CallbackEntries) do
-        CanShootHelper:RemoveCallback(v.ID, v.Fn)
+        CanShootHelper:RemoveCallback(v[1], v[3])
     end
 end
 
-CanShootHelper = RegisterMod("Can Shoot Helper", 1)
+CanShootHelper = RegisterMod("CanShoot Helper", 1)
 CanShootHelper.Internal = {}
 CanShootHelper.Internal.VERSION = VERSION
-CanShootHelper.Internal.CallbackEntries = {}
+CanShootHelper.Internal.CallbackEntries = {
+    {
+        ModCallbacks.MC_POST_PLAYER_UPDATE,
+        CallbackPriority.LATE,
+        ---@param player EntityPlayer
+        function (_, player)
+            local data = CanShootHelper.Internal:GetData(player)
 
----@param ID ModCallbacks
----@param fn function
----@param filter any
----@param priority? CallbackPriority
-function CanShootHelper.Internal:AddCallback(ID, fn, filter, priority)
-    CanShootHelper.Internal.CallbackEntries[#CanShootHelper.Internal.CallbackEntries + 1] = {
-        ID = ID,
-        Fn = fn,
-        Filter = filter,
-        Priority = priority
+            if not data.ShootTracker then
+                return
+            end
+
+            for _, v in pairs(data.ShootTracker) do
+                if v then
+                    player:SetCanShoot(false)
+                    return
+                end
+            end
+        end,
+        0
     }
+}
+
+for _, v in ipairs(CanShootHelper.Internal.CallbackEntries) do
+    CanShootHelper:AddPriorityCallback(v[1], v[2], v[3], v[4])
 end
 
 ---@param entity Entity
@@ -58,29 +68,5 @@ function CanShootHelper:Set(player, disable, identfier)
         end
 
         player:UpdateCanShoot()
-    end
-end
-
----@param player EntityPlayer
-CanShootHelper.Internal:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, function (_, player)
-    local data = CanShootHelper.Internal:GetData(player)
-
-    if not data.ShootTracker then
-        return
-    end
-
-    for _, v in pairs(data.ShootTracker) do
-        if v then
-            player:SetCanShoot(false)
-            return
-        end
-    end
-end, nil, CallbackPriority.LATE)
-
-for _, v in ipairs(CanShootHelper.Internal.CallbackEntries) do
-    if v.Priority then
-        CanShootHelper:AddPriorityCallback(v.ID, v.Priority, v.Fn, v.Filter)
-    else
-        CanShootHelper:AddCallback(v.ID, v.Fn, v.FIlter)
     end
 end
